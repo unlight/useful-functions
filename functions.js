f62196bbdc039953d4b17af68ac15ca85c5285ac
S <roman@mc.(none)>
2013-07-29 15:58:29 +0400

    Old function

--- functions.js
+++ functions.js
@@ -836,6 +836,18 @@ self.formatString = (function() {
 	};
 })();
 
+// function inherits (c, p, proto) {
+//   proto = proto || {}
+//   var e = {}
+//   ;[c.prototype, proto].forEach(function (s) {
+//     Object.getOwnPropertyNames(s).forEach(function (k) {
+//       e[k] = Object.getOwnPropertyDescriptor(s, k)
+//     })
+//   })
+//   c.prototype = Object.create(p.prototype, e)
+//   c.super = p
+// }
+
 module.exports.d = d;
 module.exports.inArray = inArray;
 module.exports.isArray = isArray;
